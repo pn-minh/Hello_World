6bbf45ff94a4cb10dcf97232d944cfc40f68e68f
pn-minh <99157746+pn-minh@users.noreply.github.com>
2022-09-08 16:14:52 +0000

    Second commit

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -3,10 +3,6 @@
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
-	int* a = new int[n];
-	for(int i = 0; i < n; i++) cin >> a[i];
-	for(int i = 0; i < n; i++) cout << a[i] << " ";
+	cout << "Hello World!" << endl;
 	return 0;
 }
